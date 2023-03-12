19327016583765eacded780f1d1f3ea115f69262
AlyxandraHarp <heyharp6@gmail.com>
2023-03-11 18:09:31 -0600

    Changes to code descriptions

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ channel = bot_data.CHANNEL
 # Generator function that yields new lines in a file
 async def follow(message) -> Iterator[str]:
     # seek the end of the file
-    async with aiofiles.open(bot_data.LOGPATH, mode='r') as file:
+    async with aiofiles.open(bot_data.LOGPATH, mode='r') as file: # async with aiofiles fixes Discord.gateway warning "Shard ID None heartbeat blocked for more than 10 seconds."
         await file.seek(0, os.SEEK_END)
         while True:
             # read last line of file
@@ -31,32 +31,23 @@ async def follow(message) -> Iterator[str]:
             if not line:
                 continue
             else:
+                # Check if new line contains text for a death message
                 if ("[Server thread/INFO]" in line) and (any((match := substring) in line for substring in bot_data.DEATH_MESSAGES)): #add <and "Named entity" not in line> to only have player deaths
-                    #this prints messages once when at least one death message is found in list
-                    print("HERE!")
                     print(f'Keyword: {match} on Line :{line.strip()}')
                     yield line.strip()
                 
 
 # Runs the blocking-heavy function is a non-blocking way
-# Reason for implimentation: Discord.gateway warning "Shard ID None heartbeat blocked for more than 10 seconds."
 async def run_blocking(blocking_func: typing.Callable, *args, **kwargs) -> typing.Any:
     func = functools.partial(blocking_func, *args, **kwargs) # `run_in_executor` doesn't support kwargs, `functools.partial` does
     return await client.loop.run_in_executor(None, func)
 
 # Read and follow log for death messages
 async def log_handler(message):
-    
-        print("before await")
-        #f = await asyncio.create_task(run_blocking(follow, file, message))
         task = await run_blocking(follow, message)
-        #a = await loop.run_until_complete(task)
-        print("after await")
         async for line in task:
-            print(line)
+            # Send latest Death to Discord channel
             await message.channel.send(f'> {line}')
-        # Check if new line contains text for a death message
-            
 
 # Send message to channel from player prompt
 async def send_message(message, user_message):
